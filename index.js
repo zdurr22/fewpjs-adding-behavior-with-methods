fa20c17259b3929d68828d7701dd5e85e6386dce
zdurr22 <zdurr22@gmail.com>
2021-10-12 11:53:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+class Cat {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    speak() {
+        return `${this.name} says meow!`
+    }
+}
+
+class Dog {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+    speak() {
+        return `${this.name} says woof!`
+    }
+}
+
+class Bird {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    speak() {
+        if (this.gender == "male") {
+            return `It's me! ${this.name}, the parrot!`
+        }
+        else {
+            return `${this.name} says squawk!`
+        }
+    }
+}
